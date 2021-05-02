290ac5e3a89bc9dafc2c78ff715d002cf890c857
suyashpradhan <pradhansuyash39@gmail.com>
2021-05-03 02:04:54 +0530

    fix: minor ui bug fixes

--- src/Components/CreateChannel/CreateChannel.jsx
+++ src/Components/CreateChannel/CreateChannel.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import "./CreateChannel.css";
-import close from '../../assests/images/close.svg'
+import close from "../../assests/images/close.svg";
 export const CreateChannel = () => {
-    const hiddenFileInput = React.useRef(null);
-  
-  const handleClick = event => {
-      event.preventDefault();
+  const hiddenFileInput = React.useRef(null);
+
+  const handleClick = (event) => {
+    event.preventDefault();
     hiddenFileInput.current.click();
   };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const fileUploaded = event.target.files[0];
     // use the fileUploaded as it contains the file uploaded
   };
   return (
     <div className="create-channel">
       <div className="create-channel-wrapper f-column">
-     <div className="close-icon-wrapper"> <img  className="close-icon"src={close} /></div>
-          <h1 className="heading text-center p2">Create A New Channel</h1>
-        {" "}
-        <form class="form wrapper">
-       
+        <div className="close-icon-wrapper">
+          {" "}
+          <img className="close-icon" src={close} alt="close" />
+        </div>
+        <h1 className="heading text-center ">Create A New Channel</h1>{" "}
+        <form class="form">
           <div class="form-column">
             <label for="name">Channel Name</label>
             <input
@@ -40,11 +41,19 @@ export const CreateChannel = () => {
           </div>
           {/* Place to add the date time input */}
           <div className="form-column">
-              <label for="file">Upload a Channel Image</label>
-              <button className="button button-default" onClick={handleClick}>Upload File</button>
-              <input type="file" id="file" ref={hiddenFileInput} onChange={handleChange} style={{display: 'none'}} />
+            <label for="file">Upload a Channel Image</label>
+            <button className="button button-default" onClick={handleClick}>
+              Upload File
+            </button>
+            <input
+              type="file"
+              id="file"
+              ref={hiddenFileInput}
+              onChange={handleChange}
+              style={{ display: "none" }}
+            />
           </div>
-          <button class="button-primary text-center">Create Channel</button>
+          <button class="button-primary text-center mB1">Create Channel</button>
         </form>
       </div>
     </div>
